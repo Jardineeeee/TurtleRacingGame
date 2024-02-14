73c95ec95e42406349e5ddc9cbc455b104a34a03
Jardine Eugene <citytea5@gmail.com>
2024-02-14 13:51:07 -0800

    Initial commit

---
+++ jardine__ex1.py
@@ -0,0 +1,12 @@
+# Class Ex 1
+# y = 2x + 3
+# where
+# x = 67, 48, 91, 77, 80
+
+for num in range(5):
+    x = int(input(" enter the value of x "))
+    y =(2 * x) + 3
+    print(' if x = ' , x, 'then y = ', y)
+    
+    
+    
